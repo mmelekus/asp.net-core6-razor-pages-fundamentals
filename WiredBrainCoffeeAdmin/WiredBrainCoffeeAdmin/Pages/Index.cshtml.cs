f9001a72f6f07226a07f6c1db633af2318b72ef4
MartinMZen <mmelekus@msn.com>
2023-09-06 14:40:47 -0500

    Added data models

--- WiredBrainCoffeeAdmin/WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public List<SurveyItem> SurveyResults { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
