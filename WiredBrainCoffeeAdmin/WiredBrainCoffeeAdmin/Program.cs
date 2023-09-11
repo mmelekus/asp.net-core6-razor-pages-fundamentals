8857351fada2707623145efde7138fa23f09f63f
MartinMZen <mmelekus@msn.com>
2023-09-11 12:50:48 -0500

    Configured to use Azure connection details

--- WiredBrainCoffeeAdmin/WiredBrainCoffeeAdmin/Program.cs
+++ WiredBrainCoffeeAdmin/WiredBrainCoffeeAdmin/Program.cs
@@ -4,12 +4,12 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<WiredContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WiredBrain")));
+builder.Services.AddDbContext<WiredContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WiredBrainAzure")));
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
